96b9f829097c8e29ffccf938a732a8ff1b5c25e2
Daryll Ko <daryll.ko@gmail.com>
2022-11-15 21:42:38 +0800

    feat: updated visuals

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,8 +4,8 @@ import "./Main.css";
 
 function gimmeGrid() {
 	let values = [];
-	for (let i = 0; i < 10; ++i) {
-		for (let j = 0; j < 20; ++j) {
+	for (let i = 1; i <= 10; ++i) {
+		for (let j = 1; j <= 20; ++j) {
 			values.push(i * j);
 		}
 	}
